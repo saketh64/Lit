a12933a52cad02cfd1cdd0a44484eebea0a3c308
Saketh Undurty <saketh.undurty@gmail.com>
2016-02-20 01:25:29 -0600

    search modal done - non responsive

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$('.add_song_button').click(function() {
+		$('.search_modal').fadeIn(200);
+	});
+
+	$('.close').click(function(){
+		$('.search_modal').fadeOut(200)
+	});
+
+
+});
