4424da789c743b6283602c6cd7fe591e7f8ae7a1
kushi.mittal@gmail.com <kushi.mittal@gmail.com>
2016-02-20 16:44:38 -0600

    Updating CSS and adding images

--- flask_app/static/js/nowplaying.js
+++ flask_app/static/js/nowplaying.js
@@ -3,3 +3,37 @@ var socket = io.connect('http://' + document.domain + ':' + location.port);
 socket.on('heartbeat_to_client', function (message) {
 	socket.emit('heartbeat_to_server', {});
 });
+
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player('video', {
+    events: {
+      // call this function when player is ready to use
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  
+  // bind events
+  var playButton = document.getElementById("play-button");
+  playButton.addEventListener("click", function() {
+    player.playVideo();
+  });
+  
+  var pauseButton = document.getElementById("pause-button");
+  pauseButton.addEventListener("click", function() {
+    player.pauseVideo();
+  });
+  
+}
