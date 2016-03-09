f7a771948da6b00a35639a878cdc10ca1fc835a9
Calvin Pelletier <calvin.pelletier@gmail.com>
2016-03-09 17:07:03 -0600

    removed extraneous methods from sesson.py and confirm that a generated user_id doesn't already exist

--- flask_app/sessions.py
+++ flask_app/sessions.py
@@ -1,27 +1,12 @@
 import random
 import string
 
-users = []
+all_user_ids = set()
 
 def get_random_user_id():
-    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
-
-
-def get_user(user_id):
-    global users
-    for user in users:
-        if user.user_id == user_id:
-            return user
-
-def does_user_exist(user_id):
-    return get_user(user_id) != None
-
-def get_all_user_activity_json():
-    global users
-    """
-    :return: a JSON object of all user activity
-    """
-    result = {}
-    for user in users:
-        result[user.user_id] = user.activity
-    return result
+    global all_user_ids
+    while 1:
+        ret = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
+        if ret not in all_user_ids:
+            all_user_ids.add(ret)
+            return ret
