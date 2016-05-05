e263c002fe02db5c0d4dcc17a28bc7df4a3eca1a
Wilson Chaney <wilsongchaney@gmail.com>
2016-05-05 16:17:28 -0500

    begin work on AudioManager

---
+++ flask_app/Core/AudioManager/main.py
@@ -0,0 +1,84 @@
+import pafy
+import os
+
+BASE_URL = "https://www.youtube.com/watch?v=%s"
+STORAGE_PATH = "flask_app/static/music/%s.mp3"
+
+class SongState:
+    DOWNLOADED = 0
+    QUEUED = 1
+    NONE = 2
+
+queue = []
+
+def get_id_from_url(url):
+    ID_idx = url.index("watch?v=")+8
+    return url[ID_idx:]
+
+def download_song(url):
+    id = get_id_from_url(url)
+    print "DOWNLOADING",id
+    if get_song_state(url) == SongState.DOWNLOADED:
+        print "SKIPPING",id
+        return
+    video = pafy.new(url)
+    stream = video.getbestaudio()
+    stream.download(STORAGE_PATH % id)
+    print "FINISHING",id
+
+def queue_song(url):
+    id = get_id_from_url(url)
+    print "APPENDING",id
+    # if queue is empty, download the song
+    if len(queue) == 0:
+        queue.append(url)
+        download_song(url)
+        queue.pop(0)
+        on_downloader_ready()
+    else:
+        queue.append(url)
+
+def on_downloader_ready():
+    if len(queue) > 0:
+        download_song(queue[0])
+        queue.pop(0)
+        on_downloader_ready()
+
+def remove_song(url):
+    id = get_id_from_url(url)
+    path = STORAGE_PATH % id
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        pass
+
+def get_song_state(url):
+    id = get_id_from_url(url)
+    path = STORAGE_PATH % id
+    if os.path.exists(path):
+        return SongState.DOWNLOADED
+    else:
+        return SongState.QUEUED
+
+
+#########################
+#       TESTING         #
+#########################
+from threading import Thread
+from time import sleep
+from random import random
+
+
+def test(url):
+    sleep(random()*2)
+    queue_song(url)
+
+urls = ["https://www.youtube.com/watch?v=LkWpURhvN3E",
+        "https://www.youtube.com/watch?v=J-vUJx8swoU",
+        "https://www.youtube.com/watch?v=f7plhrsQSEE",
+        "https://www.youtube.com/watch?v=68oooLfnoIw"]
+
+
+for u in urls:
+    t = Thread(target=test,args=(u,))
+    t.start()
